c31bab17fb1d3821ff2f43aa881ac169b0027619
jpshaw111 <jpshaw111@rediffmail.com>
2025-07-16 22:33:59 +0530

    save

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:password@localhost/fastapi_db"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:@localhost/fastapi_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
